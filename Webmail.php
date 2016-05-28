6a65f151495d186cbb880898879a9c63cea9404d
Jørgen <jorgen@heatweb.no>
2016-05-28 14:08:30 +0200

    Initial testing function

---
+++ Webmail.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author 	Jorgen Ellingsen
+ * @package jellingsen/yii2-webmail
+ * @version 0.1
+ */
+
+namespace jellingsen\webmail\Webmail;
+
+
+class Webmail
+{
+	/**
+	 * Initial Testing function
+	 * @return string
+	 */
+	public function sayHello()
+	{
+		return 'Hello';
+	}
+}
