cc01706ba87f5d2e607c9b409a47aef1f333828b
Jørgen <jorgen@heatweb.no>
2016-05-28 15:01:21 +0200

    Added ddeboer/imap package, testing connection

--- Webmail.php
+++ Webmail.php
@@ -7,15 +7,33 @@
 
 namespace jellingsen\webmail;
 
+use Ddeboer\Imap\Server;
 
 class Webmail
 {
-	/**
-	 * Initial Testing function
-	 * @return string
-	 */
-	public function sayHello()
+	private $_server;
+	private $_connection;
+
+	public function __construct($username, $password)
 	{
-		return 'Hello';
+		/* TEMP START */
+		$server = 'mail.ellera.no';
+		$port = 993;
+		$flag = '/imap/ssl/novalidate-cert';
+		$parameters = [];
+		/* TEMP END */
+
+		$this->_server = new Server($server, $port, $flag, $parameters);
+		$this->_connection = $this->_server->authenticate($username, $password);
+	}
+
+	public function showMailBoxes()
+	{
+		$mailboxes = $this->_connection->getMailboxes();
+
+		foreach ($mailboxes as $mailbox) {
+			// $mailbox is instance of \Ddeboer\Imap\Mailbox
+			printf('Mailbox %s has %s messages', $mailbox->getName(), $mailbox->count());
+		}
 	}
 }
